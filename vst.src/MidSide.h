d3242500d590cb1bfe0922733f30559ca38bb1c8
Luca Sartore <drwolf85@users.noreply.github.com>
2014-09-10 21:57:27 +0200

    Create MidSide.h

---
+++ vst.src/MidSide.h
@@ -0,0 +1,47 @@
+#ifndef __MidSide_H
+#define __MidSide_H
+
+#include "audioeffectx.h"
+
+class wolfMidSide : public AudioEffectX
+{
+  public:
+    wolfMidSide(audioMasterCallback audioMaster);
+    ~wolfMidSide();
+
+    virtual bool getEffectName(char *name);
+    virtual bool getVendorString(char *text);
+    virtual bool getProductString(char *text);
+    virtual VstInt32 getVendorVersion() { return 1000; }
+
+    virtual void setProgramName(char *name);
+    virtual void getProgramName(char *name);
+    virtual bool getProgramNameIndexed (VstInt32 category, VstInt32 index, char* name);
+
+    virtual float getParameter(VstInt32 index);
+    virtual void getParameterName(VstInt32 index, char *text);
+    virtual void setParameter(VstInt32 index, float value);
+    virtual void getParameterDisplay(VstInt32 index, char *text);
+    virtual void getParameterLabel(VstInt32 index, char *label);
+
+    virtual void suspend();
+    virtual void process(float **inputs, float **outputs, VstInt32 sampleFrames);
+    virtual void processReplacing(float **inputs, float **outputs, VstInt32 sampleFrames);
+
+
+
+  protected:
+    char programName[32];
+
+    float fParam1;
+    float fParam2;
+    float fParam3;
+    float fParam4;
+
+    float *buffer1, *buffer2;
+    int bufpos;
+    VstInt32 size;
+    VstInt32 del3, del4;
+};
+
+#endif
